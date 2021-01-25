399e63c537338b154db372e5516eb12d44d5ac89
Tobi-dev99 <talmale.vastuk5566@gmail.com>
2021-01-25 23:16:05 +0530

    hany changes
    change css designs

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,39 +13,45 @@ $(function () {
       $(this).find("span").css({ top: relY, left: relX });
     });
 
+    activatingNav();
+
 });
 
-//ENABLING FULLPAGE
-var $header_top = $(".navbar");
-var $nav = $("navbar__items");
-
-$("#fullpage").fullpage({
-  sectionsColor: ["#1c1d25", "#1c1d25", "#1c1d25", "#1c1d25", "#1c1d25"],
-  sectionSelector: ".vertical-scrolling",
-  navigation: true,
-  slideNavigation: true,
-  controlArrows: false,
-  controlArrows: false,
-  anchors: [
-    "firstSection",
-    "secondSection",
-    "thirdSection",
-    "fourthSection",
-    "fifthSection",
-  ],
-  menu: "#menu",
-
-  afterLoad: function (anchorLink, index) {
-    $header_top.css("background", "rgba(0, 47, 77, .3)");
-    $nav.css("background", "rgba(0, 47, 77, .25)");
-    if (index == 5) {
-      $("#fp-nav").hide();
+const activatingNav = function () { 
+  const op_Nav = document.querySelectorAll('.onepage-pagination li a');
+  console.log(op_Nav);
+  op_Nav.forEach(el => {
+    if (el.classList.contains('active')) {
+      console.log('true');
+      const index = el.getAttribute("data-index");
+      const currElement = document.querySelector(
+        `.navbar__link[data-index = '${index}']`
+      );
+      currElement.classList.add('navbar__link--active');
+      console.log(currElement);
+    }else{
+      const index = el.getAttribute("data-index");
+      const currElement = document.querySelector(
+        `.navbar__link[data-index = '${index}']`
+      );
+      currElement.classList.remove("navbar__link--active");
     }
-  },
+  });
+ }
 
-  onLeave: function (index, nextIndex, direction) {
-    if (index == 5) {
-      $("#fp-nav").show();
-    }
-  },
+//ENABLING FULLPage
+
+onePageScroll(".fullpage", {
+  sectionContainer: ".vertical-scrolling",
+  easing: "ease",
+  animationTime: 1000,
+  pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
+  updateURL: false, // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+  beforeMove: function () {
+    activatingNav();
+  }, // This option accepts a callback function. The function will be called before the page moves.
+  afterMove: function (index) {}, // This option accepts a callback function. The function will be called after the page moves.
+  loop: false, // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+  keyboard: true, // You can activate the keyboard controls
+  responsiveFallback: false,
 });
