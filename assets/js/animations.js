15f3a322968788230c9eb94e31f04aef18ed6429
Tobi-dev99 <59740193+Tobi-dev99@users.noreply.github.com>
2021-01-26 10:36:30 +0530

    Add files via upload

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -42,15 +42,33 @@ anime
   // });
 
 
-anime.timeline({ loop: false }).add({
-  targets: ".bg-cover",
-  opacity: [0, 1],
-  skew: ['100deg' ,0],
-  duration: 1000
-}).add({
-  targets: '.arrow-down',
-  opacity: [0, 1],
-  translateY: ['-100%', '-50%'],
-  translateX: ['-50%','-50%'],
-  duration: 1000
-})
+anime
+  .timeline({ loop: false })
+  .add({
+    targets: ".bg-cover path",
+    strokeDashoffset: [anime.setDashoffset, 0],
+    stroke: "#61892F",
+    easing: "easeInOutSine",
+    duration: 1500,
+    delay: function (el, i) {
+      return i * 250;
+    },
+  })
+  .add({
+    targets: ".bg-cover path",
+    fill: "#86C232",
+    easing: "easeInOutSine",
+    duration: 1500,
+    delay: function (el, i) {
+      return i * 250;
+    },
+  })
+  .add({
+    targets: ".arrow-down",
+    opacity: [0, 1],
+    translateY: ["-100%", "-50%"],
+    translateX: ["-50%", "-50%"],
+    duration: 1000,
+    loop:true,
+    direction:'reverse'
+  });
